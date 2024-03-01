31dc7ae22b34224264cce65fc416f3a60cca498b
Mr.Softy <123297701+MrSoftyXTrillion@users.noreply.github.com>
2024-03-01 14:55:59 +0600

    Add files via upload

---
+++ 1018 Banknotes.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main ()
+{
+    int note,n100,n50,n20,n10,n5,n2,n1;
+    scanf ("%d",&note);
+    n100 = note / 100;
+    n50 = (note % 100) / 50;
+    n20 = ((note % 100) % 50) / 20;
+    n10 = (((note % 100) % 50) % 20) / 10;
+    n5 = ((((note % 100) % 50) % 20) % 10) / 5;
+    n2 = (((((note % 100) % 50) % 20) % 10) % 5) / 2;
+    n1 = ((((((note % 100) % 50) % 20) % 10) % 5) % 2) / 1;
+    printf ("%d\n",note);
+    printf ("%d nota(s) de R$ 100,00\n",n100);
+    printf ("%d nota(s) de R$ 50,00\n",n50);
+    printf ("%d nota(s) de R$ 20,00\n",n20);
+    printf ("%d nota(s) de R$ 10,00\n",n10);
+    printf ("%d nota(s) de R$ 5,00\n",n5);
+    printf ("%d nota(s) de R$ 2,00\n",n2);
+    printf ("%d nota(s) de R$ 1,00\n",n1);
+    return 0;
+}
